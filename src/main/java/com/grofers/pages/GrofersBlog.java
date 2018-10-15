135bdc6c3c3d36d3f974432f5804c9d5c7174773
shiv.ashish@sirionlabs.com <shiv.ashish@sirionlabs.com>
2018-10-15 20:50:05 +0530

    Adding One More Test Case : That will validate the like button functionality

--- src/main/java/com/grofers/pages/GrofersBlog.java
+++ src/main/java/com/grofers/pages/GrofersBlog.java
@@ -10,95 +10,117 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 public class GrofersBlog {
 
-    Logger logger = LoggerFactory.getLogger(GrofersBlog.class);
+	Logger logger = LoggerFactory.getLogger(GrofersBlog.class);
 
-    WebDriver driver;
-    String pageUrl;
-    String baseUrl;
-    List<String> linksToValidate;
-    String facebookPageUrl;
-    String twitterPageUrl;
-    String instagramPageUrl;
-    String linkedinPageUrl;
-    int totalSocialMediaButton;
-    By pageFooter = By.xpath("//h5[contains(text(),'Grofers India Pvt. Ltd. 2016-2017')]");
-    By socialMediaButtons = By.xpath("//*[contains(text(),'Connect with us')]/..//li[@class='social']//a");
+	WebDriver driver;
+	String pageUrl;
+	String baseUrl;
+	List<String> linksToValidate;
+	String facebookPageUrl;
+	String twitterPageUrl;
+	String instagramPageUrl;
+	String linkedinPageUrl;
+	int totalSocialMediaButton;
 
-    public GrofersBlog(WebDriver driver) throws ConfigurationException {
+	By pageFooter = By.xpath("//h5[contains(text(),'Grofers India Pvt. Ltd. 2016-2017')]");
+	By socialMediaButtons = By.xpath("//*[contains(text(),'Connect with us')]/..//li[@class='social']//a");
 
-        this.driver = driver;
-        String configFilePath = ConfigureConstant.getConfigFilesValue("grofersconfigfilepath");
-        String configFileName = ConfigureConstant.getConfigFilesValue("grofersfilename");
+	By likeArticleButtons = By.xpath("//button[contains(@class,'kodex_button kodex_like_button')]");
+	By likeArticleCounters = By.xpath("//button[contains(@class,'kodex_button kodex_like_button')]//span[@class='counter']");
 
-        baseUrl = ConfigReader.getValueFromConfigFile(configFilePath, configFileName, "default", "baseurl");
-        pageUrl = ConfigReader.getValueFromConfigFile(configFilePath, configFileName, "default", "pageurl");
+	public GrofersBlog(WebDriver driver) throws ConfigurationException {
 
-        facebookPageUrl = ConfigReader.getValueFromConfigFile(configFilePath, configFileName, "socialmedia", "facebookurl");
-        twitterPageUrl = ConfigReader.getValueFromConfigFile(configFilePath, configFileName, "socialmedia", "instaurl");
-        instagramPageUrl = ConfigReader.getValueFromConfigFile(configFilePath, configFileName, "socialmedia", "twitterurl");
-        linkedinPageUrl = ConfigReader.getValueFromConfigFile(configFilePath, configFileName, "socialmedia", "linkedinurl");
-        totalSocialMediaButton = Integer.parseInt(ConfigReader.getValueFromConfigFile(configFilePath, configFileName, "socialmedia", "totalbutton"));
+		this.driver = driver;
+		String configFilePath = ConfigureConstant.getConfigFilesValue("grofersconfigfilepath");
+		String configFileName = ConfigureConstant.getConfigFilesValue("grofersfilename");
 
-        linksToValidate = Arrays.asList(ConfigReader.getValueFromConfigFile(configFilePath, configFileName, "linkstovalidate", "linktext").split(","));
-    }
+		baseUrl = ConfigReader.getValueFromConfigFile(configFilePath, configFileName, "default", "baseurl");
+		pageUrl = ConfigReader.getValueFromConfigFile(configFilePath, configFileName, "default", "pageurl");
 
-    public List<String> getLinksToValidate() {
-        return linksToValidate;
-    }
+		facebookPageUrl = ConfigReader.getValueFromConfigFile(configFilePath, configFileName, "socialmedia", "facebookurl");
+		twitterPageUrl = ConfigReader.getValueFromConfigFile(configFilePath, configFileName, "socialmedia", "instaurl");
+		instagramPageUrl = ConfigReader.getValueFromConfigFile(configFilePath, configFileName, "socialmedia", "twitterurl");
+		linkedinPageUrl = ConfigReader.getValueFromConfigFile(configFilePath, configFileName, "socialmedia", "linkedinurl");
+		totalSocialMediaButton = Integer.parseInt(ConfigReader.getValueFromConfigFile(configFilePath, configFileName, "socialmedia", "totalbutton"));
 
-    public String getFacebookPageUrl() {
-        return facebookPageUrl;
-    }
+		linksToValidate = Arrays.asList(ConfigReader.getValueFromConfigFile(configFilePath, configFileName, "linkstovalidate", "linktext").split(","));
+	}
 
-    public String getTwitterPageUrl() {
-        return twitterPageUrl;
-    }
+	public List<String> getLinksToValidate() {
+		return linksToValidate;
+	}
 
-    public String getInstagramPageUrl() {
-        return instagramPageUrl;
-    }
+	public String getFacebookPageUrl() {
+		return facebookPageUrl;
+	}
 
-    public String getLinkedinPageUrl() {
-        return linkedinPageUrl;
-    }
+	public String getTwitterPageUrl() {
+		return twitterPageUrl;
+	}
 
-    public int getTotalSocialMediaButton() {
-        return totalSocialMediaButton;
-    }
+	public String getInstagramPageUrl() {
+		return instagramPageUrl;
+	}
 
-    public String getPageUrl() {
-        return pageUrl;
-    }
+	public String getLinkedinPageUrl() {
+		return linkedinPageUrl;
+	}
 
-    public String getBaseUrl() {
-        return baseUrl;
-    }
+	public int getTotalSocialMediaButton() {
+		return totalSocialMediaButton;
+	}
+
+	public String getPageUrl() {
+		return pageUrl;
+	}
+
+	public String getBaseUrl() {
+		return baseUrl;
+	}
 
 
-    public boolean validatePageFooter() {
-        WebElement element = driver.findElement(pageFooter);
-        if (element != null)
-            return true;
-        else
-            return false;
-    }
+	public boolean validatePageFooter() {
+		WebElement element = driver.findElement(pageFooter);
+		if (element != null)
+			return true;
+		else
+			return false;
+	}
 
-    public List<WebElement> getAllSocialMediaInteractionButtons() {
-        List<WebElement> elements = driver.findElements(socialMediaButtons);
-        return elements;
-    }
+	public List<WebElement> getAllSocialMediaInteractionButtons() {
+		List<WebElement> elements = driver.findElements(socialMediaButtons);
+		return elements;
+	}
 
-    public boolean validateLinkExistance(String linkText) {
-        WebElement element = driver.findElement(By.xpath("//a[contains(text(),'" + linkText + "')]"));
-        if (element != null)
-            return true;
-        else
-            return false;
-    }
+	// this function will return map of all the like button with their counter counts
+	public LinkedHashMap<WebElement, Integer> getAllLikeArticlesButtonsWithCounterCounts() {
+		List<WebElement> buttons = driver.findElements(likeArticleButtons);
+		List<WebElement> counters = driver.findElements(likeArticleCounters);
+		LinkedHashMap<WebElement, Integer> map = new LinkedHashMap<WebElement, Integer>();
+
+		try {
+			int i = 0;
+			for (WebElement button : buttons) {
+				map.put(button, Integer.parseInt(counters.get(i++).getText()));
+			}
+		} catch (Exception e) {
+			logger.error("Error : [{}] , While Making Map of Counters Count with Like Button", e.getLocalizedMessage());
+		}
+		return map;
+	}
+
+	public boolean validateLinkExistance(String linkText) {
+		WebElement element = driver.findElement(By.xpath("//a[contains(text(),'" + linkText + "')]"));
+		if (element != null)
+			return true;
+		else
+			return false;
+	}
 
 
 }
--- src/main/java/com/grofers/test/TestGrofersBlogPage.java
+++ src/main/java/com/grofers/test/TestGrofersBlogPage.java
@@ -12,154 +12,199 @@ import org.testng.SkipException;
 import org.testng.annotations.*;
 import org.testng.asserts.SoftAssert;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 
 public class TestGrofersBlogPage {
 
 
-    Logger logger = LoggerFactory.getLogger(TestGrofersBlogPage.class);
-    WebDriver driver;
-    GrofersBlog grofersBlog;
-    SoftAssert softAssert;
-    private boolean failedInConfigReading = false;
+	Logger logger = LoggerFactory.getLogger(TestGrofersBlogPage.class);
+	WebDriver driver;
+	GrofersBlog grofersBlog;
+	SoftAssert softAssert;
+	private boolean failedInConfigReading = false;
 
 
-    @BeforeClass
-    public void beforeClass() {
-        try {
-            logger.info("Inside Before Class");
-            driver = InitializeDrivers.getDriver(ConfigureConstant.getConstantFieldsValue("browser"));
-            grofersBlog = new GrofersBlog(driver);
+	@BeforeClass
+	public void beforeClass() {
+		try {
+			logger.info("Inside Before Class");
+			driver = InitializeDrivers.getDriver(ConfigureConstant.getConstantFieldsValue("browser"));
+			grofersBlog = new GrofersBlog(driver);
 
 
-        } catch (Exception e) {
-            logger.error("Some Exception at Before Class Level : [{}]", e.getMessage());
-            failedInConfigReading = true;
-        }
+		} catch (Exception e) {
+			logger.error("Some Exception at Before Class Level : [{}]", e.getMessage());
+			failedInConfigReading = true;
+		}
 
 
-    }
+	}
 
 
-    @BeforeMethod
-    public void beforeMethod() {
-        if (!failedInConfigReading) {
-            logger.info("Inside Before Method");
-            if (driver == null) {
-                logger.error("Failed in Initializing Web Driver so skipping the test");
-                throw new SkipException("Failed in Initializing Web Driver so skipping the test");
-            }
-            softAssert = new SoftAssert();
-        } else {
-            throw new SkipException("Skipping all the tests");
-        }
-    }
+	@BeforeMethod
+	public void beforeMethod() {
+		if (!failedInConfigReading) {
+			logger.info("Inside Before Method");
+			if (driver == null) {
+				logger.error("Failed in Initializing Web Driver so skipping the test");
+				throw new SkipException("Failed in Initializing Web Driver so skipping the test");
+			}
+			softAssert = new SoftAssert();
+		} else {
+			throw new SkipException("Skipping all the tests");
+		}
+	}
 
 
-    @Test(priority = -1)
-    public void validatePageURL() {
-        String validationMessage = ValidateBrokenLinkHelper.isLinkBroken(grofersBlog.getPageUrl());
+	@Test(priority = -1)
+	public void validatePageURL() {
+		String validationMessage = ValidateBrokenLinkHelper.isLinkBroken(grofersBlog.getPageUrl());
 
-        if (validationMessage.toLowerCase().contains("ok")) {
-            driver.get(grofersBlog.getPageUrl());
-            softAssert.assertTrue(grofersBlog.validatePageFooter(), "Error : Footer Message is not there ");
-        } else {
-            logger.error("validation Message is [{}]", validationMessage);
-            softAssert.assertTrue(false, "Error : Page Url has been broken");
-        }
+		if (validationMessage.toLowerCase().contains("ok")) {
+			driver.get(grofersBlog.getPageUrl());
+			softAssert.assertTrue(grofersBlog.validatePageFooter(), "Error : Footer Message is not there ");
+		} else {
+			logger.error("validation Message is [{}]", validationMessage);
+			softAssert.assertTrue(false, "Error : Page Url has been broken");
+		}
 
-        softAssert.assertAll();
+		softAssert.assertAll();
 
-    }
-
-
-    @Test(dependsOnMethods = "validatePageURL" , priority = 1)
-    public void validateAllLinksInPage() {
-
-        driver.get(grofersBlog.getPageUrl());
-        List<WebElement> allLinks = SeleniumWebDriverCommonHelper.findAllLinks(driver);
-        logger.debug("Total number of elements found " + allLinks.size());
+	}
 
 
-        for (WebElement element : allLinks) {
+	@Test(dependsOnMethods = "validatePageURL", priority = 1)
+	public void validateAllLinksInPage() {
 
-            if (element.getAttribute("href").startsWith(grofersBlog.getBaseUrl())) {
-                String validationMessage = ValidateBrokenLinkHelper.isLinkBroken(element.getAttribute("href"));
-                logger.debug("URL: " + element.getAttribute("href") + " returned " + validationMessage);
-
-                if (!validationMessage.toLowerCase().contains("ok")) {
-                    logger.error("validation Message is [{}] for Url [{}]", validationMessage, element.getAttribute("href"));
-                    softAssert.assertTrue(false, "Error : " + element.getAttribute("href") + " Url has been broken" + " Response Message is : " + validationMessage + "\n");
-                }
-            }
-        }
+		driver.get(grofersBlog.getPageUrl());
+		List<WebElement> allLinks = SeleniumWebDriverCommonHelper.findAllLinks(driver);
+		logger.debug("Total number of elements found " + allLinks.size());
 
 
-        softAssert.assertAll();
+		for (WebElement element : allLinks) {
 
-    }
+			if (element.getAttribute("href").startsWith(grofersBlog.getBaseUrl())) {
+				String validationMessage = ValidateBrokenLinkHelper.isLinkBroken(element.getAttribute("href"));
+				logger.debug("URL: " + element.getAttribute("href") + " returned " + validationMessage);
 
-    @Test(dependsOnMethods = "validatePageURL", priority = 2)
-    public void validateSocialMediaInteractionButtons() {
-
-        driver.get(grofersBlog.getPageUrl());
-        List<WebElement> allButtons = grofersBlog.getAllSocialMediaInteractionButtons();
-        logger.info("Total number of elements found " + allButtons.size());
-
-        if (allButtons.size() != 0 && allButtons.size() == grofersBlog.getTotalSocialMediaButton()) {
-            for (WebElement button : allButtons) {
-
-                if (button.getAttribute("class").toLowerCase().contains("facebook")) {
-                    softAssert.assertTrue(button.getAttribute("href").contains(grofersBlog.getFacebookPageUrl()), "Facebook : Interaction URL is incorrect");
-                } else if (button.getAttribute("class").toLowerCase().contains("instagram")) {
-                    softAssert.assertTrue(button.getAttribute("href").contains(grofersBlog.getInstagramPageUrl()), "Instagram : Interaction URL is incorrect");
-                } else if (button.getAttribute("class").toLowerCase().contains("twitter")) {
-                    softAssert.assertTrue(button.getAttribute("href").contains(grofersBlog.getTwitterPageUrl()), "Twitter : Interaction URL is incorrect");
-                } else if (button.getAttribute("class").toLowerCase().contains("linkedin")) {
-                    softAssert.assertTrue(button.getAttribute("href").contains(grofersBlog.getLinkedinPageUrl()), "LinkedIn : Interaction URL is incorrect");
-                }
-            }
-        } else {
-            softAssert.assertFalse(false, "Any Or All Social Media Interaction Buttons not there in Current page");
-        }
+				if (!validationMessage.toLowerCase().contains("ok")) {
+					logger.error("validation Message is [{}] for Url [{}]", validationMessage, element.getAttribute("href"));
+					softAssert.assertTrue(false, "Error : " + element.getAttribute("href") + " Url has been broken" + " Response Message is : " + validationMessage + "\n");
+				}
+			}
+		}
 
 
-        softAssert.assertAll();
+		softAssert.assertAll();
 
-    }
+	}
 
-    @Test(dependsOnMethods = "validatePageURL", priority = 3)
-    public void validateAllMandatoryLinks() {
+	@Test(dependsOnMethods = "validatePageURL", priority = 2)
+	public void validateSocialMediaInteractionButtons() {
 
-        driver.get(grofersBlog.getPageUrl());
-        List<String> allMandatoryLinksText = grofersBlog.getLinksToValidate();
+		driver.get(grofersBlog.getPageUrl());
+		List<WebElement> allButtons = grofersBlog.getAllSocialMediaInteractionButtons();
+		logger.info("Total number of elements found " + allButtons.size());
 
-        for(String linkText : allMandatoryLinksText)
-        {
-            softAssert.assertTrue(grofersBlog.validateLinkExistance(linkText),linkText + " : Link is missing from page  ");
-        }
+		if (allButtons.size() != 0 && allButtons.size() == grofersBlog.getTotalSocialMediaButton()) {
+			for (WebElement button : allButtons) {
+
+				if (button.getAttribute("class").toLowerCase().contains("facebook")) {
+					softAssert.assertTrue(button.getAttribute("href").contains(grofersBlog.getFacebookPageUrl()), "Facebook : Interaction URL is incorrect");
+				} else if (button.getAttribute("class").toLowerCase().contains("instagram")) {
+					softAssert.assertTrue(button.getAttribute("href").contains(grofersBlog.getInstagramPageUrl()), "Instagram : Interaction URL is incorrect");
+				} else if (button.getAttribute("class").toLowerCase().contains("twitter")) {
+					softAssert.assertTrue(button.getAttribute("href").contains(grofersBlog.getTwitterPageUrl()), "Twitter : Interaction URL is incorrect");
+				} else if (button.getAttribute("class").toLowerCase().contains("linkedin")) {
+					softAssert.assertTrue(button.getAttribute("href").contains(grofersBlog.getLinkedinPageUrl()), "LinkedIn : Interaction URL is incorrect");
+				}
+			}
+		} else {
+			softAssert.assertFalse(false, "Any Or All Social Media Interaction Buttons not there in Current page");
+		}
 
 
-        softAssert.assertAll();
+		softAssert.assertAll();
 
-    }
-
-
-
-    @AfterMethod
-
-    public void afterMethod() {
-        logger.info("Inside After Method");
-
-    }
+	}
 
 
-    @AfterClass
-    public void afterClass() {
-        logger.info("Inside After Class");
-        driver.quit();
-    }
+	@Test(dependsOnMethods = "validatePageURL", priority = 3)
+	public void validateAllMandatoryLinks() {
+
+		driver.get(grofersBlog.getPageUrl());
+		List<String> allMandatoryLinksText = grofersBlog.getLinksToValidate();
+
+		for (String linkText : allMandatoryLinksText) {
+			softAssert.assertTrue(grofersBlog.validateLinkExistance(linkText), linkText + " : Link is missing from page  ");
+		}
+
+
+		softAssert.assertAll();
+
+	}
+
+	@Test(dependsOnMethods = "validatePageURL", priority = 4)
+	public void validateLikeButtonFunctionality() throws InterruptedException {
+
+		driver.get(grofersBlog.getPageUrl());
+		LinkedHashMap<WebElement, Integer> allLikeButtonsWithCountersCount = grofersBlog.getAllLikeArticlesButtonsWithCounterCounts();
+
+		if (allLikeButtonsWithCountersCount.size() > 0) {
+			List<Integer> previousCounts = new ArrayList<Integer>();
+			for (WebElement likebutton : allLikeButtonsWithCountersCount.keySet()) {
+				previousCounts.add(allLikeButtonsWithCountersCount.get(likebutton));
+				likebutton.click(); // Click on the Like Button
+				Thread.sleep(2000); // although it should not be used , used for testing purpose
+			}
+
+			driver.get(driver.getCurrentUrl()); // refreshing the page
+
+			LinkedHashMap<WebElement, Integer> allUpdatedLikeButtonsWithCountersCount = grofersBlog.getAllLikeArticlesButtonsWithCounterCounts();
+			List<Integer> newCounts = new ArrayList<Integer>(allUpdatedLikeButtonsWithCountersCount.values());
+
+			if (previousCounts.size() == newCounts.size()) {
+
+				for (int i = 0; i < previousCounts.size(); i++) {
+					if (newCounts.get(i) != (previousCounts.get(i) + 1)) {
+						softAssert.assertTrue(false, "Error : Click On Like Button Functionality is not working : Count is not properly Getting Updated for " + i + "th Like Button");
+
+
+					}
+				}
+
+			} else {
+				logger.debug("Error :Size of List having Counters Counts is not equal : Skipping the validation ");
+				throw new SkipException("Error :Size of List having Counters Counts is not equal : Skipping the validation ");
+			}
+
+		} else {
+			logger.error("Error : Article Having Like Button Not Found ");
+			throw new SkipException("Error : Article Having Like Button Not Found  ");
+		}
+
+
+		softAssert.assertAll();
+
+	}
+
+
+	@AfterMethod
+
+	public void afterMethod() {
+		logger.info("Inside After Method");
+
+	}
+
+
+	@AfterClass
+	public void afterClass() {
+		logger.info("Inside After Class");
+		driver.quit();
+	}
 
 
 }
