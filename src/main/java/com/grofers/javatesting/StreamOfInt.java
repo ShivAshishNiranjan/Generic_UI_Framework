8b80cecb7de9f04f5585ac61694c8e46c4a7c5b8
Shiv Ashish <shivashish.niranjan@grofers.com>
2019-11-04 13:27:21 +0530

    For Loop Replacement

--- src/main/java/com/grofers/javatesting/StreamOfInt.java
+++ src/main/java/com/grofers/javatesting/StreamOfInt.java
@@ -1,16 +1,19 @@
 package com.grofers.javatesting;
 
+import com.google.gson.internal.bind.util.ISO8601Utils;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 
 /**
  * @author : shiv.ashish@grofers.com
  * Contact me on san.mnnit11@gmail.com or 8105234517 for any query
  * file created on 04/11/19
  */
-public class IntStream {
+public class StreamOfInt {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
         Collections.addAll(list,1,2,3,4,5,6,7,8,9);
@@ -22,6 +25,8 @@ public class IntStream {
 
 
         System.out.println("Sum is ::"+ sum);
+
+        IntStream.range(0,10).forEach(s -> System.out.println("Hare Krishna") );
 
 
     }
